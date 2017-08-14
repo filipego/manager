eb40d69c8f0ef6ca81d15925102e20139ffbb081
Filipe Valente Gomes <filipego@Filipes-MBP.fios-router.home>
2017-08-14 17:11:55 -0400

    added firebase

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyB9MN2c_Jh6H0Ut266EYI9WwZdL3rER6gc',
+      authDomain: 'manager-7c33b.firebaseapp.com',
+      databaseURL: 'https://manager-7c33b.firebaseio.com',
+      projectId: 'manager-7c33b',
+      storageBucket: 'manager-7c33b.appspot.com',
+      messagingSenderId: '598938197029'
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
