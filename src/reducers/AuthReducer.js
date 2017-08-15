4da6325bfbbc20bc0c82783cfee7c16957ef1127
Filipe Valente Gomes <filipego@Filipes-MacBook-Pro.local>
2017-08-15 17:35:57 -0400

    added login functionality

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,20 +1,31 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCES } from '../actions/types';
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCES,
+  LOGIN_USER_FAIL,
+  LOGIN_USER
+} from '../actions/types';
 
 const INITIAL_STATE = {
   email: '',
   password: '',
-  user: null
+  loading: false,
+  user: null,
+  error: ''
 };
 
 export default (state = INITIAL_STATE, action) => {
-  console.log(action);
   switch (action.type) {
     case EMAIL_CHANGED:
       return { ...state, email: action.payload };
     case PASSWORD_CHANGED:
       return { ...state, password: action.payload };
+    case LOGIN_USER:
+      return { ...state, loading: true, error: '' };
     case LOGIN_USER_SUCCES:
-      return { ...state, user: action.payload };
+      return { ...state, ...INITIAL_STATE, user: action.payload };
+    case LOGIN_USER_FAIL:
+      return { ...state, error: 'Authentication Failed.', password: '', loading: false };
     default:
       return state;
   }
